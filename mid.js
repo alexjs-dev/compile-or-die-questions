b26901f2cf5a3fb9f53ffb5b49cb7395df018029
Aleksei D <z-alex-z@hotmail.com>
2025-04-11 15:25:26 +0300

    feat: update

--- mid.js
+++ mid.js
@@ -1,20 +1,20 @@
 var mid = [
-    {
-        code: [
-            "defmodule Math do",
-                "def square_all(list), do: Enum.map(list, fn x -> x * x end)",
-                "square_all([1, 2, :three])",
-            "end"
-        ],
-        error: "** (ArithmeticError) you cannot multiply :three with :three",
-        challenge_type: ChallengeType.PICK_LINE,
-        error_line: 3,
-        is_solution_valid: false,
-        success_response: {
-          message: "You're too dangerous to be left alive!",
-        },
-        failure_response: {
-          message: "And to think you're the ones who developed AI?!",
-        },
-      },
+  {
+    code: [
+      "defmodule Math do",
+      "def square_all(list), do: Enum.map(list, fn x -> x * x end)",
+      "square_all([1, 2, :three])",
+      "end",
+    ],
+    error: "** (ArithmeticError) you cannot multiply :three with :three",
+    challenge_type: ChallengeType.PICK_LINE,
+    error_line: 3,
+    is_solution_valid: false,
+    success_response: {
+      message: "FALLBACK",
+    },
+    failure_response: {
+      message: "FALLBACK",
+    },
+  },
 ];
